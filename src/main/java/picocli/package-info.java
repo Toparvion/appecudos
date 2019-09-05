4522028529a5df51da9ca856a2f1be4756353293
Vladimir Plizga <v.plizga@cft.ru>
2019-09-05 13:00:19 +0700

    Applied Gradle 'application' plugin in order to: 1. Make portable distribution 2. Extract (back) picocli from sources to libraries

--- src/main/java/picocli/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains a copy of classes from 'info.picocli:picocli:*' Gradle artifact. The purpose of the copying 
- * is to make the toolchain as light-weight as possible by avoiding external dependencies.
- * @author Toparvion
- */
-package picocli;
